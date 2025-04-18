9416d95456eec46d48d1ceae4bd329c82f4ba957
lishifeng <lishifenging@outlook.com>
2025-04-19 00:04:41 +0800

    fix: fix doc errors

--- doc/mainpage/mainpage.h
+++ doc/mainpage/mainpage.h
@@ -1,10 +1,10 @@
 /**
  * @mainpage  LibDriver SHT35
- * @brief     LibDriver SHT35 is a full function driver of SHT35 and it can run in both MCU and Linux platform.
+ * @brief     LibDriver SHT35 is a full-featured driver of SHT35 and it can run in both MCU and Linux platform.
  * @version   2.0.0
  * @author    Shifeng Li
  *
- * <h3>history</h3>
+ * <h3>History</h3>
  * <table>
  * <tr><th>Date        <th>Version  <th>Author      <th>Description
  * <tr><td>2021/03/05  <td>2.0      <td>Shifeng Li  <td>format the code
